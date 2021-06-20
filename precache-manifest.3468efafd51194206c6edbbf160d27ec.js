951bc9b2c20c135ab237df115d6382a10f6c40da
Gaurav Habad <gauravhabad113@gmail.com>
2021-06-21 00:49:58 +0530

    Updates

---
+++ precache-manifest.3468efafd51194206c6edbbf160d27ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a97072b4e26ed8fce8eb41415477da2d",
+    "url": "/Hacker-News/index.html"
+  },
+  {
+    "revision": "977de0766cd8b1952511",
+    "url": "/Hacker-News/static/css/main.9c59207b.chunk.css"
+  },
+  {
+    "revision": "2829f658d05344f4543e",
+    "url": "/Hacker-News/static/js/2.4904b4fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Hacker-News/static/js/2.4904b4fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "977de0766cd8b1952511",
+    "url": "/Hacker-News/static/js/main.42ad7d17.chunk.js"
+  },
+  {
+    "revision": "155bd69c3f81282751a6",
+    "url": "/Hacker-News/static/js/runtime-main.cf2f6047.js"
+  }
+]);
